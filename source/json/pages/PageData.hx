0c0d407d2cd039087d8b08a087c5bfec10441ddb
IdealistCat <mattallan.h@gmail.com>
2024-12-27 16:40:17 -0600

    update PageData

--- source/json/pages/PageData.hx
+++ source/json/pages/PageData.hx
@@ -11,11 +11,17 @@ class PageData
     @:default("Unknown")
     public var title:String;
 
+    /**
+    * the page objects (i.e a textField or a button, maybe an image)
+    */
+    @:default([])
+    public var objects:String;
+
 	/**
 	 * Produces a string representation suitable for debugging.
 	 */
 	public function toString()
 	{
-		return 'PageData(title: ${this.title})';
+		return 'PageData(title: ${this.title}, objects: ${this.objects})';
 	}
 }
