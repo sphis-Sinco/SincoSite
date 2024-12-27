09f3e290cef274f6106429b09de5fc1174404515
IdealistCat <mattallan.h@gmail.com>
2024-12-27 16:07:53 -0600

    Inital Commit

---
+++ source/import.hx
@@ -0,0 +1,10 @@
+import flixel.*;
+import flixel.graphics.atlas.FlxAtlas;
+import flixel.math.*;
+import flixel.sound.*;
+import flixel.text.*;
+import flixel.tweens.*;
+import flixel.tweens.misc.*;
+import flixel.tweens.motion.*;
+import flixel.util.*;
+import main.*;
---
+++ source/main/MainState.hx
@@ -0,0 +1,14 @@
+package;
+
+class MainState extends FlxState
+{
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
